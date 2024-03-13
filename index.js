f10b754d58d4a1ce06e59349dcc95984e8ec3249
Alx78370 <alexis.vachet@outlook.fr>
2024-03-13 20:21:47 +0100

    fixed menu burger effect

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
-
-const burgeropen = document.getElementById('icons').innerHTML = "☰";
+const burgeropen = (document.getElementById("icons").innerHTML = "☰");
 const links = document.querySelectorAll("nav li");
 
 icons.addEventListener("click", () => {
   nav.classList.toggle("active");
   if (nav.classList.contains("active")) {
-  const burgerclose = document.getElementById('icons').innerHTML = "✘";
-  }
-  else {
-    const burgeropen = document.getElementById('icons').innerHTML = "☰";           
+    const burgerclose = (document.getElementById("icons").innerHTML = "✘");
+  } else {
+    const burgeropen = (document.getElementById("icons").innerHTML = "☰");
   }
 });
 
 links.forEach((link) => {
   link.addEventListener("click", () => {
     nav.classList.remove("active");
-    const burgeropen = document.getElementById('icons').innerHTML = "☰";
+    const burgeropen = (document.getElementById("icons").innerHTML = "☰");
   });
 });
 
-function sendMail()
-{
-  const subjectLine = document.getElementById("name").value + " " + document.getElementById("surname").value;
+function sendMail() {
+  const subjectLine =
+    document.getElementById("name").value +
+    " " +
+    document.getElementById("surname").value;
   const body = document.getElementById("Msg").value;
-  window.location.href ="mailto:alexis.vachet@outlook.fr?subject="+subjectLine+"&body="+body;
+  window.location.href =
+    "mailto:alexis.vachet@outlook.fr?subject=" + subjectLine + "&body=" + body;
 }
